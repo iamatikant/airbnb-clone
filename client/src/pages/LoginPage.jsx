f7f911cf9cc41674d42975d0cab9cb684e6c7d36
iamatikant <atikant.ati@gmail.com>
2024-02-05 00:41:22 +0530

    Login changes

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,13 +1,50 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, Navigate } from "react-router-dom";
 
 export default function LoginPage() {
+  const [email, setEmail] = useState("sujesh@gmail.com");
+  const [password, setPassword] = useState("password");
+  const [redirect, setRedirect] = useState(false);
+
+  const handleLoginSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await fetch('/login', {
+        method: 'POST',
+        body: JSON.stringify({ email, password }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      alert('Login successful', await response.json());
+      setRedirect(true);
+    } catch(e) {
+      alert('Login failed', e);
+    }
+  }
+
+  if(redirect) {
+    // setRedirect(false);
+    return <Navigate to={'/'} />
+  }
+
   return (
     <div className="mt-4 grow flex items-center justify-around">
       <div className="mb-64">
         <h1 className="text-4xl text-center mb-4">Login</h1>
-        <form className="max-w-md mx-auto">
-          <input type="email" placeholder="youremail@email.com" />
-          <input type="password" placeholder="password" />
+        <form className="max-w-md mx-auto" onSubmit={handleLoginSubmit}>
+          <input
+            type="email"
+            placeholder="youremail@email.com"
+            value={email}
+            onChange={(ev) => setEmail(ev.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="password"
+            value={password}
+            onChange={(ev) => setPassword(ev.target.value)}
+          />
           <button className="primary">Login</button>
           <div className="text-gray-300 py-4 text-center">
             Don't have an account yet?{" "}
